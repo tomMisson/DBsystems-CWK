4804cc74528edb46b472b1ecf5b4438d2ca60370
Tom <11tmisson@gmail.com>
2020-03-10 13:45:41 +0000

    Final create table script

--- Migration/CreateNewSysTables.sql
+++ Migration/CreateNewSysTables.sql
@@ -1,3 +1,12 @@
+DROP TABLE GENRES CASCADE CONSTRAINTS ;
+DROP TABLE NAME CASCADE CONSTRAINTS ;
+DROP TABLE PROFESSIONS CASCADE CONSTRAINTS ;
+DROP TABLE PROFESSION_NAMES CASCADE CONSTRAINTS ;
+DROP TABLE TITLE CASCADE CONSTRAINTS ;
+DROP TABLE Title_Cast CASCADE CONSTRAINTS ;
+DROP TABLE TITLE_GENRE CASCADE CONSTRAINTS ;
+DROP TABLE KNOWNFOR CASCADE CONSTRAINTS ;
+
 CREATE TABLE Title (
      TitleID varchar(9) PRIMARY KEY,
      Title varchar(255),
@@ -7,7 +16,7 @@ CREATE TABLE Title (
 );
 
 CREATE TABLE Name(
-    NameID varchar(9),
+    NameID varchar(9) PRIMARY KEY,
     Name varchar(100),
     BirthYear number(4),
     DeathYear number(4)
@@ -18,7 +27,18 @@ CREATE TABLE Genres(
     Genre varchar(32)
 );
 
-CREATE TABLE Tile_Genre(
+CREATE TABLE Professions(
+    ProfessionID number(4) PRIMARY KEY,
+    Profession varchar(32)
+);
+
+CREATE TABLE Title_Cast(
+    CharacterPlayed varchar(32) PRIMARY KEY,
+    TitleID varchar(9) REFERENCES Title(TitleID),
+    NameID varchar(9) REFERENCES Name(NameID)
+);
+
+CREATE TABLE Title_Genre(
     GenreID number(3) REFERENCES Genres(GenreID),
     TitleId varchar(9) REFERENCES Title(TitleID),
     PRIMARY KEY (GenreID, TitleId)
@@ -30,19 +50,8 @@ CREATE TABLE KnownFor(
     PRIMARY KEY (NameID, TitleID)
 );
 
-CREATE TABLE Title_Cast(
-    CharacterPlayed varchar(32) PRIMARY KEY,
-    TitleID varchar(9)REFERENCES Title(TitleID),
-    NameID varchar(9) REFERENCES Name(NameID)
-);
-
-CREATE TABLE Professions(
-    ProfessionID number(4) PRIMARY KEY,
-    Profession varchar(32)
-);
-
 CREATE TABLE Profession_Names(
-    NameID varchar(9) REFERENCES Names(NameID),
+    NameID varchar(9) REFERENCES Name(NameID),
     ProfessionID number(4) REFERENCES Professions(ProfessionID),
     PRIMARY KEY(NameID,ProfessionID)
 );
