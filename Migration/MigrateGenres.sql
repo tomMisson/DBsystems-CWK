42c6a580da31b6e530a8da0770d1ca615574d77c
Thomas Misson <11tmisson@gmail.com>
2020-03-24 15:35:58 +0000

    Migrate all genres across

---
+++ Migration/MigrateGenres.sql
@@ -0,0 +1,9 @@
+INSERT INTO genres (Genre)
+SELECT genres FROM
+(
+    SELECT GENRE1 AS genres FROM otitles 
+    UNION 
+    SELECT GENRE2 AS genres FROM otitles 
+    UNION 
+    SELECT GENRE3 AS genres FROM  otitles
+) A
